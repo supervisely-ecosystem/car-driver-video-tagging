fee86dabfd914f7f8e592c5a55dcedc2d0d8385e
max kolomeychenko <maxim.kolomeychenko@gmail.com>
2021-10-28 10:56:07 +0300

    start UI

--- src/main.py
+++ src/main.py
@@ -1,51 +1,36 @@
-import os
-import random
-import string
+import sly_globals as g
+
 import supervisely_lib as sly
 
-
-my_app = sly.AppService()
-
-LENGTH = int(os.environ['modal.state.length'])
-
-
-@my_app.callback("generate")
-@sly.timeit
-def generate_random_string(api: sly.Api, task_id, context, state, app_logger):
-    rand_string = ''.join((random.choice(string.ascii_letters + string.digits)) for _ in range(LENGTH))
-    rand_string = state["prefix"] + rand_string
-    api.task.set_field(task_id, "data.randomString", rand_string)
-
-
-@my_app.callback("preprocessing")
-@sly.timeit
-def preprocessing(api: sly.Api, task_id, context, state, app_logger):
-    sly.logger.info("do something here")
+# @my_app.callback("generate")
+# @sly.timeit
+# def generate_random_string(api: sly.Api, task_id, context, state, app_logger):
+#     return
 
 
 def main():
-    sly.logger.info("Script arguments from modal dialog box",  extra={"length: ": LENGTH})
+    sly.logger.info("Script arguments",  extra={}) # "length: ": LENGTH
 
     api = sly.Api.from_env()
 
     data = {
-        "randomString": "hello!"
+        "userId": g.user_id,
+        "userLogin": g.user_login
     }
 
     state = {
-        "prefix": "abc_"
     }
 
     initial_events = [
-        {
-            "state": None,
-            "context": None,
-            "command": "preprocessing",
-        }
+        # {
+        #     "state": None,
+        #     "context": None,
+        #     "command": "preprocessing",
+        # }
     ]
 
     # Run application service
-    my_app.run(data=data, state=state, initial_events=initial_events)
+    g.my_app.run(data=data, state=state, initial_events=initial_events)
 
 
 if __name__ == "__main__":
---
+++ src/sly_globals.py
@@ -0,0 +1,36 @@
+import os
+from pathlib import Path
+import sys
+from dotenv import load_dotenv
+import supervisely_lib as sly
+
+
+root_source_dir = str(Path(sys.argv[0]).parents[1])
+print(f"App source directory: {root_source_dir}")
+sys.path.append(root_source_dir)
+
+# only for convenient debug
+debug_env_path = os.path.join(root_source_dir, "debug.env")
+secret_debug_env_path = os.path.join(root_source_dir, "secret_debug.env")
+load_dotenv(debug_env_path)
+load_dotenv(secret_debug_env_path, override=True)
+
+my_app = sly.AppService()
+api = my_app.public_api
+task_id = my_app.task_id
+
+#@TODO: for debug
+#sly.fs.clean_dir(my_app.data_dir)
+
+team_id = int(os.environ['context.teamId'])
+workspace_id = int(os.environ['context.workspaceId'])
+project_id = int(os.environ['modal.state.slyProjectId'])
+
+user_id = int(os.environ['context.userId'])
+user_login = os.environ['context.userLogin']
+
+project_info = api.project.get_info_by_id(project_id)
+if project_info is None:  # for debug
+    raise ValueError(f"Project with id={project_id} not found")
+project_meta = sly.ProjectMeta.from_json(api.project.get_meta(project_id))
+
